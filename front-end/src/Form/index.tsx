23fddb85b89ab1bd0f1c66394fc2f514db341d2f
Rafael Crespim <rafa.cechinato@gmail.com>
2024-02-11 18:32:48 -0300

    start forms

--- front-end/src/Form/index.tsx
+++ front-end/src/Form/index.tsx
@@ -1,8 +1,28 @@
 import { useContext, useEffect } from 'react'
 import { Context } from '../Context'
 import './style.css'
+import styled from 'styled-components';
 
-function App() {
+interface FormProps {
+  dark?: boolean;
+}
+
+const FormContainer = styled.form<FormProps>`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  margin: auto;
+  background-color: ${(props) => (props.dark ? '#242424' : 'white')}
+  border: 1px solid white;
+`;
+
+const Input = styled.input`
+  padding: 8px;
+  margin-bottom: 12px;
+  width: 100%;
+`;
+
+function Form() {
   const {allContext,setAllContext} = useContext<any>(Context);
 
   useEffect(() => {
@@ -12,10 +32,10 @@ function App() {
   
 
   return (
-    <>
-      
-    </>
+    <FormContainer onSubmit={() => {}}>
+      <Input type="text" placeholder="Name" />
+    </FormContainer>
   )
 }
 
-export default App
+export default Form
